3cd2bd77a0aec653d9c6e52b62138d8ce30ddb8c
USER001 <lironlevy03@gmail.com>
2026-02-13 10:14:54 +0200

    commit

--- app/src/main/java/com/example/ex9/MainActivity.java
+++ app/src/main/java/com/example/ex9/MainActivity.java
@@ -8,12 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.EditText;
+
 public class MainActivity extends AppCompatActivity {
+    EditText etn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etn=findViewById(R.id.etn);
+
+    }
+
+    public void clickme(View view) {
 
     }
 }
